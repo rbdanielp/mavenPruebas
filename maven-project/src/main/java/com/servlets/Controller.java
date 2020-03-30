a04de52f6fad2775addf72406bf3a656f642c1a4
daniel <daniel@daniel-HP-Pavilion-m7-Notebook-PC>
2020-03-29 21:14:33 -0300

    LOG

--- maven-project/src/main/java/com/servlets/Controller.java
+++ maven-project/src/main/java/com/servlets/Controller.java
@@ -1,39 +1,91 @@
 package com.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Servlet implementation class Controller
  */
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Controller() {
-        super();
-        // TODO Auto-generated constructor stub 
-    }
+
+	private static final Logger logger = LogManager.getLogger(Controller.class);
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Controller() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		logger.info(" ");
+		logger.info(" ");
+		logger.info("			=======");
+		logger.info("			DO GET ");
+		logger.info("			=======");
+		logger.info(" ");
+		logger.info(" ");
+
+		String accion = getAccion(request);
+//		try {
+//			switch (accion) {
+//			case value:
+//				
+//				break;
+//
+//			default:
+//				break;
+//			}
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//		}
+
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		logger.info(" ");
+		logger.info(" ");
+		logger.info("			=======");
+		logger.info("			DO POST");
+		logger.info("			=======");
+		logger.info(" ");
+		logger.info(" ");
 		// TODO Auto-generated method stub
 		doGet(request, response);
+	}
+
+	//////////////////////////////////////////////////////////////////////////////
+
+	// *********************************************************************************
+	// GET ACCION
+	// *********************************************************************************
+	private String getAccion(HttpServletRequest req) {
+		String accion = req.getRequestURI().substring(req.getContextPath().length() + 1, req.getRequestURI().length());
+		logger.info(" ");
+		logger.info("			Accion: " + accion);
+		logger.info(" ");
+		return accion;
 	}
 
 }
