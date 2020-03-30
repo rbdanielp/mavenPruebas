976cadd1c567a3ddfaba3f4005d6a44e0faa0e08
daniel <daniel@daniel-HP-Pavilion-m7-Notebook-PC>
2020-03-29 22:02:58 -0300

    Otro Servlet

---
+++ maven-project/src/main/java/com/servlets/Parametros.java
@@ -0,0 +1,85 @@
+package com.servlets;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.*;
+import java.util.*;
+
+public class Parametros extends HttpServlet {
+
+	private static final Logger logger = LogManager.getLogger(Parametros.class);
+
+	public void init(ServletConfig conf) throws ServletException {
+		super.init(conf);
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		logger.info(" ");
+		logger.info(" ");
+		logger.info("			=======");
+		logger.info("			DO GET ");
+		logger.info("			=======");
+		logger.info(" ");
+		logger.info(" ");
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		// Escribimos el principio de la página HTML
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Parámetros del servlet desde un formulario HTML</h1>");
+
+		// cogemos los nombres de los parametros
+		Enumeration paramNames = request.getParameterNames();
+
+		// vamos mostrando los parámetros en unwhile
+		while (paramNames.hasMoreElements()) {
+			// cogemos el siguiente parámetro
+			String paramName = (String) paramNames.nextElement();
+
+			// Mostramos el nombre del parámetro
+			out.print(paramName + " = ");
+
+			// Cogemos los valores del parámetro
+			String[] paramValues = request.getParameterValues(paramName);
+
+			// Miramos si tiene más de un valor
+			if (paramValues.length == 1) {
+				// Si tiene un sólo valor, miramos si está vacío o no
+				String paramValue = paramValues[0];
+				if (paramValue.length() == 0)
+					out.println("<i>Sin valor</i><br>");
+				else
+					out.println(paramValue + "<br>");
+			} else {
+				// Si tiene más de un sólo valor, los mostramos
+				for (int i = 0; i < paramValues.length; i++)
+					out.println(paramValues[i] + ", ");
+				out.println("<br>");
+			}
+		} // end while
+
+		// Escribimos el final de la página HTML
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		logger.info(" ");
+		logger.info(" ");
+		logger.info("			=======");
+		logger.info("			DO POST");
+		logger.info("			=======");
+		logger.info(" ");
+		logger.info(" ");
+		
+		doGet(request, response);
+	}
+}
---
+++ maven-project/src/main/webapp/invocacionPost.jsp
@@ -0,0 +1,38 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+   <h1>Formulario</h1>
+   <form method="POST" action="/maven-project/servlets/Parametros">
+     Nombre: <INPUT TYPE="TEXT" NAME="nombre"><BR>
+     Primer Apellido:<INPUT TYPE="TEXT" NAME="apellido1"><BR>
+     Segundo Apellido:<INPUT TYPE="TEXT" NAME="apellido2"><BR>
+     <hr>
+     Correo electronico: <INPUT TYPE="TEXT" NAME="email"><BR>
+     Clave: <INPUT TYPE="PASSWORD" NAME="clave"><BR>
+     <hr>
+     Comentario: <TEXTAREA NAME="comenta" ROWS=3 COLS=40>
+                 </TEXTAREA><BR>
+     <hr>
+     Sexo:<BR> 
+	  <INPUT TYPE="RADIO" NAME="sexo" VALUE="hombre">Hombre<BR>
+          <INPUT TYPE="RADIO" NAME="sexo" VALUE="mujer">Mujer<BR>
+
+     Areas de interés:<br>
+  	  <SELECT NAME="intereses" MULTIPLE>
+	 	<OPTION>Informatica</OPTION>
+	  	<OPTION>Derecho</OPTION>
+		<OPTION>Matematicas</OPTION>
+		<OPTION>Fisica</OPTION>
+		<OPTION>Musica</OPTION>
+	  </SELECT>
+
+      <center><input type="submit" value="Enviar"></center>
+   </form>
+</body>
+</html>
