a757ace787f10ed5559f3c569f4e9cb004e9385f
daniel <daniel@daniel-HP-Pavilion-m7-Notebook-PC>
2020-04-01 15:55:32 -0300

    Ajuste de Formatos

--- maven-project/src/main/java/com/servlets/Controller.java
+++ maven-project/src/main/java/com/servlets/Controller.java
@@ -19,62 +19,61 @@ import com.negocio.Fachada;
 // INVOCACION 
 // http://localhost:8080/maven-project/Controller?accion=123
 
-
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private static final Logger logger = LogManager.getLogger(Controller.class);
-	private static final String ACCION_POST= "invocacionPost.do";
-	private static final String ACCION_GET= "invocacionGet.do";
-	private static final String ACCION_OPERACION_CON_FECHA= "operacionConFecha.do";
+
+	// ACCIONES
+	private static final String ACCION_POST = "invocacionPost.do";
+	private static final String ACCION_GET = "invocacionGet.do";
+	private static final String ACCION_OPERACION_CON_FECHA = "operacionConFecha.do";
 
 	public Controller() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
+	// *********************************************************************************
+	//
+	// *********************************************************************************
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		Fachada.logTitulo("Controller: DO GET");
-		
 
 		String accion = getAccion(request);
 		try {
 			switch (accion) {
 			case ACCION_POST:
-				//logger.debug("Controller: switch: ACCION_POST");
+				// logger.debug("Controller: switch: ACCION_POST");
 				Fachada.logTitulo("ACCION_POST");
 				break;
 			case ACCION_GET:
-				//logger.debug("Controller: switch: ACCION_GET");
+				// logger.debug("Controller: switch: ACCION_GET");
 				Fachada.logTitulo("ACCION_GET");
-			break;	
+				break;
 			case ACCION_OPERACION_CON_FECHA:
-				//logger.debug("Controller: switch: ACCION_OPERACION_CON_FECHA");
+				// logger.debug("Controller: switch: ACCION_OPERACION_CON_FECHA");
 				Fachada.logTitulo("ACCION_OPERACION_CON_FECHA");
-				
+
 				SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-				
+
 				int horas = 0;
 				horas = Integer.parseInt(request.getParameter("horas"));
-				
+
 				Date dNow = new Date();
 				String fechaActual = sdf.format(dNow);
-				logger.debug("fechaActual: " + fechaActual );
-				
-				logger.debug("horas: " + horas );
-				Date nuevaF = Fachada.sumarRestarHorasFecha(dNow, horas);
-			
-				String nuevaFecha = sdf.format(nuevaF);
-				logger.debug("nuevaFecha: " + nuevaFecha );
-				
-				escibirFechaRespuesta(response,  nuevaFecha);
+				logger.debug("fechaActual: " + fechaActual);
 
-			break;				
+				logger.debug("horas: " + horas);
+				Date nuevaF = Fachada.sumarRestarHorasFecha(dNow, horas);
+
+				String nuevaFecha = sdf.format(nuevaF);
+				logger.debug("nuevaFecha: " + nuevaFecha);
+
+				escibirFechaRespuesta(response, nuevaFecha);
+
+				break;
 
 			default:
 				break;
@@ -87,28 +86,27 @@ public class Controller extends HttpServlet {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
+	// *********************************************************************************
+	//
+	// *********************************************************************************
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		Fachada.logTitulo("Controller: DO POST");
-		
+
 		String accion = getAccion(request);
-//		try {
-//			switch (accion) {
-//			case value:
-//				
-//				break;
-//
-//			default:
-//				break;
-//			}
-//		} catch (Exception e) {
-//			// TODO: handle exception
-//		}
-		
+		// try {
+		// switch (accion) {
+		// case value:
+		//
+		// break;
+		//
+		// default:
+		// break;
+		// }
+		// } catch (Exception e) {
+		// // TODO: handle exception
+		// }
+
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
@@ -119,43 +117,41 @@ public class Controller extends HttpServlet {
 	// GET ACCION
 	// *********************************************************************************
 	private String getAccion(HttpServletRequest req) {
-		//String accion = req.getRequestURI().substring(req.getContextPath().length() + 1, req.getRequestURI().length());
+		// String accion = req.getRequestURI().substring(req.getContextPath().length() +
+		// 1, req.getRequestURI().length());
 		String accion = req.getParameter("accion");
-		
+
 		Fachada.logTitulo("Controller: get Accion");
 		return accion;
 	}
-	
-	
+
 	// *********************************************************************************
-	// 
+	//
 	// *********************************************************************************
 	private Enumeration getParametrosOperacionesFecha(HttpServletRequest req) {
 		Enumeration paramOperacionesFecha = req.getParameterNames();
-		
+
 		Fachada.logTitulo("Controller: getParametrosOperacionesFecha");
 		return paramOperacionesFecha;
 	}
-	
-	
+
 	// *********************************************************************************
-	// 
+	//
 	// *********************************************************************************
-	private void  escibirFechaRespuesta(HttpServletResponse response, String nuevaFecha) throws IOException {
-		
+	private void escibirFechaRespuesta(HttpServletResponse response, String nuevaFecha) throws IOException {
+
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<body>");
 		out.println("<h1>Resultado</h1><br>");
 		out.println("<br>");
-		
-		
+
 		out.print("Nueva Fecha: " + nuevaFecha);
-		
+
 		out.println("<br>");
 		out.println("<br>");
 		out.println("<br>");
-		
+
 		out.println("</body>");
 		out.println("</html>");
 	}
